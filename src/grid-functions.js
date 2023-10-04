ee1ff758a96112b3ece7901a5cbbdb252669fa74
Ray <rayoneill.97@gmail.com>
2023-10-04 09:16:47 -0400

    Added functionality to resize canvas to a maximum of 100x100 squares

--- src/grid-functions.js
+++ src/grid-functions.js
@@ -4,40 +4,37 @@ let changeSizeButton = document.getElementById("change-size-button");
 
 
 init_canvas(16);
+init_buttons();
 
 function init_canvas(size) {
-    if (size > 100) {
-        console.log("Number too large");
-        return;
-    }
-
     let totalSize = size**2;
-    if (isNaN(totalSize)) {
-        console.log("Not a valid number");
-        return;
-    }
 
+    let width_height = main_grid.clientHeight/size;
+    
     for (let i = 0; i < totalSize; ++i) {
         let grid_item = document.createElement("div");
         grid_item.setAttribute("class", "grid-item");
+        grid_item.style.height = `${width_height}px`;
+        grid_item.style.width = `${width_height}px`;
         grid_item.addEventListener("click", function(e) {
             change_background(e.target, "red");
         });
         main_grid.appendChild(grid_item);
     }
-    init_buttons();
 }
 
 function clear_canvas() {
-    let nodes = main_grid.getElementsByClassName("grid-item");
+    let nodes = Array.from(main_grid.getElementsByClassName("grid-item"));
     if (nodes === null || nodes.length === 0) {
         return;
     }
 
-    nodes.array.forEach(element => {
+    nodes.forEach(element => {
         main_grid.removeChild(element);
-    })
+    });
 }
+
+// Reset canvas to blank slate
 function reset_canvas() {
     let children = Array.from(main_grid.getElementsByClassName("grid-item"));
 
@@ -46,18 +43,34 @@ function reset_canvas() {
     });
 }
 
+// Resize canvas after clearing
 function resize_canvas(size) {
+    if (size > 100) {
+        console.log("Number too large");
+        return;
+    }
+
+    if (isNaN(size**2)) {
+        console.log("Not a valid number");
+        return;
+    }
+
+    console.log("clearing...");
     clear_canvas();
-    init_canvas(size**2)
+    init_canvas(size);
 }
 
 function init_buttons() {
     clearCanvasButton.addEventListener("click", () => { reset_canvas() });
-    changeSizeButton.addEventListener("click", function() { init_canvas(get_new_size()); } );    
+    changeSizeButton.addEventListener("click", function() { 
+        let size = get_new_size();
+        resize_canvas(size);
+    });    
 }
 
 function change_background(grid, color) {
     grid.style.background = color; 
+    console.log(window.getComputedStyle(grid, ':hover'));
 }
 
 function get_new_size() {
